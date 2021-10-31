1b61daeccaafc87cb31927c3cb29a1f1e319a585
shenliubao <1069395062@qq.com>
2021-10-31 22:27:31 +0800

    shell

---
+++ testx.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+read -a arr
+for((i=0;i<${#arr[@]};++i))
+do
+   for((j=${#arr[@]}-1;j>i;--j))
+   do
+       if [[ ${arr[j]} -lt ${arr[j-1]} ]]             
+	then
+            tmp=${arr[j]};
+            arr[j]=${arr[j-1]};
+            arr[j-1]=$tmp;
+        fi
+    done
+done
+
+echo ${arr[@]}
